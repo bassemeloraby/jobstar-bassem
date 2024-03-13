d5e236cbbfeea37cfe6413918b925093c9acab21
bassem <basem12683@gmail.com>
2024-03-13 16:47:42 +0300

    small sidebar 1

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar';
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   return (
-    <Wrapper>SmallSidebar</Wrapper>
-  )
-}
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={() => console.log("toggle")}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default SmallSidebar
+export default SmallSidebar;
